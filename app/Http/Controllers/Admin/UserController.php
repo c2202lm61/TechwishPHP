9c5c088ebada1d52d3107e976dd50e41a3ec357f
phamquan <c2202lm.pmquan@aptech.vn>
2023-08-10 21:02:21 +0700

    add

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -13,28 +13,35 @@ class UserController extends Controller
         $users = User::all();
         return view('Admin.UserManagement',compact('users'));
     }
-    public function create(){
-        return redirect('rrrrrr');
-    }
     public function insert(Request $request){
-        $user =  new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->phone = $request->phone;
-        $user->RoleID = 1;
-     //   $user->password = Hash::$request->password;
-        $user->save();
-        return "Insert thanh cong";
+        if ($request->isMethod('get')) {
+            return view('Admin.Create.CreateUser');
+        }elseif ($request->isMethod('post')) {
+            $user =  new User;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->phone = $request->phone;
+            $user->RoleID = 1;
+        //   $user->password = Hash::$request->password;
+            $user->save();
+            return "Insert thanh cong";
+        }
     }
-    public function update(){
-        $user =  User::find(1);
-        $user->name = "PhamQuan1";
-        $user->email = "quanqqq11@gmail.com";
-        $user->phone = "0322233878";
-        $user->RoleID = 1;
-        $user->password = "qqqq111";
-        $user->save();
+    public function update(Request $request){
+        if ($request->isMethod('get')) {
+            return view('Admin.Update.UpdateUser');
+        }elseif ($request->isMethod('post')) {
+            $user =  User::find(1);
+            $user->name = "PhamQuan1";
+            $user->email = "quanqqq11@gmail.com";
+            $user->phone = "0322233878";
+            $user->RoleID = 1;
+            $user->password = "qqqq111";
+            $user->save();
         return "Update thanh cong";
+            return "Insert thanh cong";
+        }
+
     }
     public function delete(Request $request){
         $user =  User::find($request->id);
